839f1eef92b64a56f88360efe878def74ea30aaa
savindaJ <thantrige32@gmail.com>
2024-06-24 10:47:29 +0530

    add service registry

--- TicketWay-ServiceRegistry/src/main/java/lk/ijse/ticketway/registry/TicketWayServiceRegistryApplication.java
+++ TicketWay-ServiceRegistry/src/main/java/lk/ijse/ticketway/registry/TicketWayServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package lk.ijse.ticketway.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class TicketWayServiceRegistryApplication {
 
     public static void main(String[] args) {
