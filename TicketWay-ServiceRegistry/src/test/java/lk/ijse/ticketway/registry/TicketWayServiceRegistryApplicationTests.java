dfef7f14744f010fb823edaba8ce88f7e237cf9c
savindaJ <thantrige32@gmail.com>
2024-06-24 10:06:40 +0530

    initial commit

---
+++ TicketWay-ServiceRegistry/src/test/java/lk/ijse/ticketway/registry/TicketWayServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.ticketway.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketWayServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
