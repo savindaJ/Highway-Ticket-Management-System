5649ff975fc9fc4ff5f026915dc782d77e54b43a
savindaJ <thantrige32@gmail.com>
2024-06-24 10:07:31 +0530

    initial commit

---
+++ TicketWay-ConfigServer/src/main/java/lk/ijse/ticketway/configserver/TicketWayConfigServerApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.ticketway.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketWayConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketWayConfigServerApplication.class, args);
+    }
+
+}
