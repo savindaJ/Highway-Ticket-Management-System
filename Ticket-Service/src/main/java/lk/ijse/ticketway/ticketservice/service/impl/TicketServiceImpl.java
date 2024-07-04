9dda523fe5d26a24f6fce18c3a37a7df63329ab6
savindaJ <thantrige32@gmail.com>
2024-07-04 09:57:59 +0530

    add collections json files

--- Ticket-Service/src/main/java/lk/ijse/ticketway/ticketservice/service/impl/TicketServiceImpl.java
+++ Ticket-Service/src/main/java/lk/ijse/ticketway/ticketservice/service/impl/TicketServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * @author : savindaJ
@@ -27,6 +28,8 @@ public class TicketServiceImpl implements TicketService {
     private final ModelMapper mapper;
 
     private final RestTemplate restTemplate;
+
+    private final Logger logger = Logger.getLogger(TicketServiceImpl.class.getName());
 
     public TicketServiceImpl(TicketRepository ticketRepository, ModelMapper mapper, RestTemplate restTemplate) {
         this.ticketRepository = ticketRepository;
@@ -58,12 +61,14 @@ public class TicketServiceImpl implements TicketService {
         });
 
         ticketRepository.save(map);
+        logger.info("Ticket Saved Successfully");
         return new ResponseDTO("Ticket saved successfully", 200);
     }
 
     @Override
     public ResponseDTO update(TicketDTO ticketDTO) {
         ticketRepository.save(mapper.map(ticketDTO, Ticket.class));
+        logger.info("Ticket Updated Successfully");
         return new ResponseDTO("Ticket updated successfully", 200);
     }
 
@@ -72,6 +77,7 @@ public class TicketServiceImpl implements TicketService {
         List<TicketDTO> all = ticketRepository.findAll().stream().map(ticket -> mapper.map(ticket, TicketDTO.class)).toList();
         HashMap<String, Object> objectObjectHashMap = new HashMap<>();
         objectObjectHashMap.put("tickets", all);
+        logger.info("Ticket Fetched Successfully");
         return new ResponseDTO(200, "Ticket fetched successfully", objectObjectHashMap);
     }
 
@@ -81,6 +87,7 @@ public class TicketServiceImpl implements TicketService {
         HashMap<String, Object> objectObjectHashMap = new HashMap<>();
         TicketDTO map = mapper.map(ticket, TicketDTO.class);
         objectObjectHashMap.put("ticket", map);
+        logger.info("Ticket Fetched Successfully");
         return new ResponseDTO(200, "Ticket fetched successfully", objectObjectHashMap);
     }
 }
